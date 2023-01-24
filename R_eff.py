e8bfdd151d3709feb1b1f3555f4d3596e16f1e67
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2023-01-24 15:10:43 -0800

    individual main files

--- R_eff.py
+++ R_eff.py
@@ -6,15 +6,13 @@ import ga_func
 seeds = [12, 13, 14, 15, 16]
 
 # Varying parameter(s)
-task = 'num_inc'
-params = np.arange(100, 1200, 200, dtype=int)
-# cuts = params + 3.0
+task = 'R_eff'
+params = np.arange(3.0, 14.0, 2.0)
+cuts = params + 3.0
 
 # Constants
 num_total = 200
-# num_inc = 200
-sigma = 5
-cut = 8
+num_inc = 200
 mass_inc = 1.0
 num_cycle = 10
 ts = 50000
@@ -22,18 +20,19 @@ anneal_temp = 2000
 
 # Job settings
 nodes = 8
-time = 25
+time = 35
 
 # Main code
 os.makedirs(task, exist_ok=True)
 for index, param in enumerate(params):
     folder_list = []
     for seed in seeds:
-        _, in_prefix, folder = ga_func.gen_ga(taskname=task, num_total=num_total, num_inc=param,
-                                              mass_inc=mass_inc, sigma=sigma, cut=cut,
+        _, in_prefix, folder = ga_func.gen_ga(taskname=task, num_total=num_total, num_inc=num_inc,
+                                              mass_inc=mass_inc, sigma=param, cut=cuts[index],
                                               num_cycle=num_cycle, ts=ts, anneal_temp=anneal_temp, seed=seed)
         folder_list.append(folder)
     # Write shell script to run many tasks
     filename = ga_func.write_sh(task, folder_list, in_prefix, param, nodes, time)
     shutil.move(filename, f'{task}/{filename}')
 os.system(f"scp -r {task}/ bwzheng@login.expanse.sdsc.edu:/expanse/lustre/scratch/bwzheng/temp_project/paper/")
+shutil.rmtree(task)
---
+++ num_inc.py
@@ -0,0 +1,38 @@
+import os, shutil
+import numpy as np
+import ga_func
+
+
+seeds = [12, 13, 14, 15, 16]
+
+# Varying parameter(s)
+task = 'num_inc'
+params = np.arange(100, 1200, 200, dtype=int)
+
+# Constants
+num_total = 200
+sigma = 5
+cut = 8
+mass_inc = 1.0
+num_cycle = 10
+ts = 50000
+anneal_temp = 2000
+
+# Job settings
+nodes = 8
+time = 35
+
+# Main code
+os.makedirs(task, exist_ok=True)
+for index, param in enumerate(params):
+    folder_list = []
+    for seed in seeds:
+        _, in_prefix, folder = ga_func.gen_ga(taskname=task, num_total=num_total, num_inc=param,
+                                              mass_inc=mass_inc, sigma=sigma, cut=cut,
+                                              num_cycle=num_cycle, ts=ts, anneal_temp=anneal_temp, seed=seed)
+        folder_list.append(folder)
+    # Write shell script to run many tasks
+    filename = ga_func.write_sh(task, folder_list, in_prefix, param, nodes, time)
+    shutil.move(filename, f'{task}/{filename}')
+os.system(f"scp -r {task}/ bwzheng@login.expanse.sdsc.edu:/expanse/lustre/scratch/bwzheng/temp_project/paper/")
+shutil.rmtree(task)
